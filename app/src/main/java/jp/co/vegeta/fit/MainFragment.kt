7df6469871e91c32a4d4e974bc916f440d58f262
vegeta2102 <hoangvmh@gmail.com>
2021-01-30 13:18:22 +0900

    Move screen

--- app/src/main/java/jp/co/vegeta/fit/MainFragment.kt
+++ app/src/main/java/jp/co/vegeta/fit/MainFragment.kt
@@ -3,16 +3,29 @@ package jp.co.vegeta.fit
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.vegeta.fit.databinding.FragmentMainBinding
+import kotlinx.android.synthetic.main.fragment_main.*
 
 @AndroidEntryPoint
 class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private val navigation: NavController by lazy {
+        findNavController()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         FragmentMainBinding.bind(view).apply {
             lifecycleOwner = viewLifecycleOwner
+        }
+        lifecycleScope.launchWhenCreated {
+            excel_reading.setOnClickListener {
+                navigation.navigate(R.id.main_to_excel_reading)
+            }
         }
     }
 }
--- feature/excel_reading/src/main/java/jp/co/vegeta/excel_reading/view/ExcelReadingFragment.kt
+++ feature/excel_reading/src/main/java/jp/co/vegeta/excel_reading/view/ExcelReadingFragment.kt
@@ -1,8 +1,19 @@
 package jp.co.vegeta.excel_reading.view
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import jp.co.vegeta.excel_reading.R
+import jp.co.vegeta.excel_reading.databinding.FragmentExcelReadingBinding
+
 /**
  * Created by vegeta on 2021/01/29.
  */
 
-class ExcelReadingFragment {
+class ExcelReadingFragment : Fragment(R.layout.fragment_excel_reading) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FragmentExcelReadingBinding.bind(view)
+    }
 }
