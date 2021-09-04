4394ae179a607535303a45e3d8ee053e2b78f0d7
vegeta2102 <hoangvmh@gmail.com>
2021-09-04 14:28:24 +0900

    Fix ktlint

--- feature/medium/src/main/java/jp/co/vegeta/medium/view/NextScreenFragment.kt
+++ feature/medium/src/main/java/jp/co/vegeta/medium/view/NextScreenFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.NavController
